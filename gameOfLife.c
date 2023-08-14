3b642ff00da70e9635bc114fae90418fb2af36b1
Grant <acdcgrr@gmail.com>
2023-08-13 23:50:18 -0500

    fixed rand() not picking high enough numbers

--- gameOfLife.c
+++ gameOfLife.c
@@ -10,76 +10,75 @@
 #define HEIGHT     (SCREEN_H / CELL_WIDTH)
 #define ARR_SIZE   (HEIGHT * WIDTH)
 
-bool curr_g[ARR_SIZE];
-bool next_g[ARR_SIZE];
-bool* curr_ptr = curr_g;
-bool* next_ptr = next_g;
+static bool sCellArr1[ARR_SIZE];
+static bool sCellArr2[ARR_SIZE];
+static bool* sCurrentGen = sCellArr1;
+static bool* sNextGen = sCellArr2;
 
-void boundsClamp(int* x, int* y) {
-    if ((*x) >= WIDTH)  (*x) -= WIDTH;
-    else if ((*x) < 0)  (*x) += WIDTH;
-    if ((*y) >= HEIGHT) (*y) -= HEIGHT;
-    else if ((*y) < 0)  (*y) += HEIGHT;
+//If the coords go out of bounds, 
+//then wrap them to the other side of the screen.
+void boundsWrapAround(int* x, int* y) {
+    if (*x >= WIDTH)  *x -= WIDTH;
+    else if (*x < 0)  *x += WIDTH;
+    if (*y >= HEIGHT) *y -= HEIGHT;
+    else if (*y < 0)  *y += HEIGHT;
 }
 
-int indexFromCoords(int x, int y, bool isRead) {
-	if (isRead) boundsClamp(&x,&y);
+int indexFromCoords(int x, int y, bool shouldDoBoundsCheck) {
+	if (shouldDoBoundsCheck) boundsWrapAround(&x,&y);
     return WIDTH * y + x;
 }
 
 int main(void) {
     InitWindow(SCREEN_W, SCREEN_H, "game of life");
     SetTargetFPS(7);
-       
-    //generate rng alive cells
-    srand(time(NULL));
-    int num_of_starter_cells = ARR_SIZE / 10;
-    for (int i = 0; i < num_of_starter_cells; i++) {        
-        int rng = rand() % ARR_SIZE;
-        curr_g[rng] = 1;
+    
+    //generate random alive starting cells
+    srand((unsigned)time(NULL));
+    int numStartingCells = ARR_SIZE / 10;
+    for (int i = 0; i < numStartingCells; ++i) {        
+        int rand_x = rand() % WIDTH, rand_y = rand() % HEIGHT;
+        sCellArr1[indexFromCoords(rand_x, rand_y, false)] = 1;
     }
 
     while (!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(BLACK);
-     
-        for (int i = 0; i < ARR_SIZE; i++) {
+        
+        for (int i = 0; i < ARR_SIZE; ++i) {
             int x = i % WIDTH;
             int y = i / HEIGHT;
-            int currIndex = indexFromCoords(x, y, 0);
-            int alive_neighbors = 0;
+            int aliveNeighbors = 0;
 
-            if (curr_ptr[indexFromCoords(x    , y - 1, 1)]) alive_neighbors++;
-            if (curr_ptr[indexFromCoords(x - 1, y - 1, 1)]) alive_neighbors++;
-            if (curr_ptr[indexFromCoords(x - 1, y    , 1)]) alive_neighbors++;
-            if (curr_ptr[indexFromCoords(x - 1, y + 1, 1)]) alive_neighbors++;
-            if (curr_ptr[indexFromCoords(x    , y + 1, 1)]) alive_neighbors++;
-            if (curr_ptr[indexFromCoords(x + 1, y + 1, 1)]) alive_neighbors++;
-            if (curr_ptr[indexFromCoords(x + 1, y    , 1)]) alive_neighbors++;
-            if (curr_ptr[indexFromCoords(x + 1, y - 1, 1)]) alive_neighbors++;
+            if (sCurrentGen[indexFromCoords(x    , y - 1, true)]) ++aliveNeighbors;
+            if (sCurrentGen[indexFromCoords(x - 1, y - 1, true)]) ++aliveNeighbors;
+            if (sCurrentGen[indexFromCoords(x - 1, y    , true)]) ++aliveNeighbors;
+            if (sCurrentGen[indexFromCoords(x - 1, y + 1, true)]) ++aliveNeighbors;
+            if (sCurrentGen[indexFromCoords(x    , y + 1, true)]) ++aliveNeighbors;
+            if (sCurrentGen[indexFromCoords(x + 1, y + 1, true)]) ++aliveNeighbors;
+            if (sCurrentGen[indexFromCoords(x + 1, y    , true)]) ++aliveNeighbors;
+            if (sCurrentGen[indexFromCoords(x + 1, y - 1, true)]) ++aliveNeighbors;
 
-            if (alive_neighbors >= 4 || alive_neighbors <= 1) {
-                next_ptr[indexFromCoords(x, y, 0)] = 0;
-            } else if (alive_neighbors == 3) {
-                next_ptr[indexFromCoords(x, y, 0)] = 1;
-            } else {
-                next_ptr[currIndex] = curr_ptr[currIndex];
-            }                   
+            if (aliveNeighbors >= 4 || aliveNeighbors <= 1) {sNextGen[i] = false;}
+            else if (aliveNeighbors == 3) {sNextGen[i] = true;}
+            else {sNextGen[i] = sCurrentGen[i];}                          
         }
-        //draw cells
+        
+        //draw cells  
+        BeginDrawing();
+        ClearBackground(BLACK); 
         int x = 0, y = 0;
-        for (int i = 0; i < ARR_SIZE; i++) {
-            if (next_ptr[i]) {
+        for (int i = 0; i < ARR_SIZE; ++i) {
+            if (sNextGen[i]) {
                 x = (i % WIDTH) * CELL_WIDTH;
                 y = (i / HEIGHT) * CELL_WIDTH;
                 DrawRectangle(x, y, CELL_WIDTH, CELL_WIDTH, WHITE);
             }
         }
-        //swap pointers
-        bool* ptr_buffer = curr_ptr;
-        curr_ptr = next_ptr;
-        next_ptr = ptr_buffer;
-     
+        
+        //swap cell arrays
+        bool* temp_ptr = sCurrentGen;
+        sCurrentGen = sNextGen;
+        sNextGen = temp_ptr;
+
         EndDrawing();
     }
 }
